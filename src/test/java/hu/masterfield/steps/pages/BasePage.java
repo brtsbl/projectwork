c0cfb1e09d48089d5a453fd43c36de4c4e96b454
brtsbl <bartos.abel@gmail.com>
2024-07-17 02:56:10 +0200

    basepage

---
+++ src/test/java/hu/masterfield/steps/pages/BasePage.java
@@ -0,0 +1,21 @@
+/* Selenium Webdriver elindítása */
+@BeforeAll
+public static void setup() {
+    // set chrome options
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+
+    // init driver
+    driver = new ChromeDriver(chromeOptions);
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    driver.manage().window().setSize(new Dimension(900, 900));
+}
+
+@AfterAll
+public static void cleanup() {
+    driver.quit();
+}
+protected static WebDriver driver;
+
+protected static WebDriverWait wait;
