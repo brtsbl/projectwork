37fa05a60029fd8042189a7f6587d3b27ae82852
brtsbl <bartos.abel@gmail.com>
2024-07-17 12:15:48 +0200

    basepage

--- src/test/java/hu/masterfield/steps/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/steps/TescoSteps.java
@@ -1,12 +1,16 @@
 package hu.masterfield.steps.steps;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 /* Selenium Webdriver elindítása */
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.openqa.selenium.WebDriver;
 import java.time.Duration;
 public class TescoSteps {
 
@@ -31,4 +35,37 @@ public static void setup() {
 public static void cleanup() {
     driver.quit();
 }
+
+    @Given("I open the home page")
+    public void iOpenTheHomePage() {
+    }
+
+    @When("I search for {string}")
+    public void iSearchFor(String arg0) {
+    }
+
+    @Then("{string} product\\(s) is\\/are displayed")
+    public void productSIsAreDisplayed(String arg0) {
+    }
+
+    @And("the product's name contains {string}")
+    public void theProductSNameContains(String arg0) {
+    }
+
+    @And("header contains the word {string}")
+    public void headerContainsTheWord(String arg0) {
+    }
+
+    @Then("nothing changes on the page")
+    public void nothingChangesOnThePage() {
+    }
+
+    @Then("a message displayed that the search didn't find relevant products")
+    public void aMessageDisplayedThatTheSearchDidnTFindRelevantProducts() {
+    }
+
+    @And("{string} button is available")
+    public void buttonIsAvailable(String arg0) {
+    }
+
 }
